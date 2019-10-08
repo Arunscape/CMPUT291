5a834f21aba540323f99fca51711d394ffdb9ea2
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-10-08 12:55:17 -0600

    first sql command

---
+++ assignments/Assignment2/1.sql
@@ -0,0 +1,8 @@
+SELECT persons.fname, persons.lname, persons.phone
+FROM persons, registrations, vehicles
+WHERE persons.fname=registrations.fname
+AND persons.lname=registrations.lname
+AND registrations.vin=vehicles.vin
+AND vehicles.make='Chevrolet'
+AND vehicles.model='Camaro'
+AND vehicles.year=1969
