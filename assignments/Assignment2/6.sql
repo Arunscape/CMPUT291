2b362d8237e827aa776e55cb660961f95073c503
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-10-08 20:20:10 -0600

    do query 6

--- assignments/Assignment2/6.sql
+++ assignments/Assignment2/6.sql
@@ -1,1 +1,11 @@
 .print Question 6 - awoosare
+SELECT persons.fname, persons.lname
+FROM persons
+JOIN marriages ON 
+	(persons.fname=marriages.p1_fname AND persons.lname=marriages.p1_lname
+		AND marriages.p2_fname='Michael' AND marriages.p2_lname='Fox')
+	OR
+	(persons.fname=marriages.p2_fname AND persons.lname=marriages.p2_lname
+		AND marriages.p1_fname='Michael' AND marriages.p1_lname='Fox')
+ORDER BY marriages.regdate DESC
+LIMIT 1
