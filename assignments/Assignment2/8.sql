17bf4732c5a8efc6932a315f73bef065fd13b7a3
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-10-10 20:05:00 -0600

    do question 8

--- assignments/Assignment2/8.sql
+++ assignments/Assignment2/8.sql
@@ -1,33 +1,30 @@
 .print Question 8 - awoosare
 -- For each year of a car, find the most frequent make and the most frequent car color. In case of ties, list all those ties.
-SELECT v.year, v.make, v.color
-FROM vehicles v
-
--- ORDER BY num_make DESC
-GROUP BY v.year
-HAVING 
-COUNT(v.make) = (SELECT COUNT(vehicles.make) AS vc
-                    from vehicles
-                    GROUP BY vehicles.make
-                    ORDER BY vc DESC
-                    LIMIT 1) -- number of the most frequent make
-AND
-COUNT(v.model) = (SELECT COUNT(vehicles.model) AS vm
-                    from vehicles
-                    GROUP BY vehicles.model
-                    ORDER BY vm DESC
-                    LIMIT 1) -- number of the most frequent model
-;
-
-
--- (SELECT COUNT(vehicles.make) AS vc
---                     from vehicles
---                     GROUP BY vehicles.make
---                     ORDER BY vc DESC
---                     LIMIT 1) -- most frequent make
-
--- (SELECT vehicles.model, COUNT(vehicles.model) AS vm
---                     from vehicles
---                     GROUP BY vehicles.make
---                     ORDER BY vm DESC
---                     LIMIT 1) -- most frequent model                    
+SELECT year, make, color
+FROM (
+        (SELECT v.year, v.make
+            FROM vehicles v
+            GROUP BY v.year, v.make
+            -- for each year, get the make(s) which have the max count for that year
+            HAVING COUNT(*) = (SELECT MAX(make_count)
+                                FROM
+                                    (SELECT year, make, COUNT(*) as make_count
+                                        FROM vehicles
+                                    GROUP BY year, make)
+                                WHERE year = v.year) -- for each year, get the count for each make
+        ) make_table -- now we have the most popular make for each year including ties
+        -- do the same thing again for colour and JOIN it on year
+        LEFT OUTER JOIN
+            (SELECT veh.year, veh.color
+                FROM vehicles veh
+                GROUP BY veh.year, veh.color
+                HAVING COUNT(*) = (SELECT MAX(colour_count)
+                                    FROM
+                                        (SELECT year, color, COUNT(*) as colour_count
+                                            FROM vehicles
+                                            GROUP BY year, color) 
+                                            WHERE year = veh.year)
+            ) colour_table
+        USING(year) -- same column name
+    );
+-- FINALLY, select the year, make, color from this clusterf***
