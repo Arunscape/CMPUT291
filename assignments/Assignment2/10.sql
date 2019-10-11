7c7695dba11daf9ea05802f953e36607c7466556
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-10-11 10:17:24 -0600

    10 works but 9 doesn't somehow lol

--- assignments/Assignment2/10.sql
+++ assignments/Assignment2/10.sql
@@ -1,15 +1,17 @@
 .print Question 10 - awoosare
 -- Using the view created in Q9, for every person who has received at least 3 different tickets within the past year and one of those tickets involves a 'red light' violation 
 -- (i.e. 'red light' appears in the violation text, e.g. 'red light crossing', 'crossing red light at 114 St and 87 Ave'), list the name of the person and the make and the model of the car for which the red violation ticket is given.
-SELECT fname, lname, make, model
+SELECT pd.fname, pd.lname, v.make, v.model
 FROM
-personDetails
-
-HAVING COUNT(*) =  (SELECT DISTINCT COUNT(*) as num_of_tickets
-                    FROM tickets
-                    WHERE num_of_tickets >=3
-                    AND violation LIKE '%red light%'
-                    GROUP BY tno 
-        
-GROUP BY person
+personDetails pd
+JOIN registrations r ON r.fname=pd.fname AND r.lname=pd.lname
+-- USING(fname, lname)
+JOIN vehicles v ON v.vin=r.vin
+-- USING(vin)
+JOIN tickets t ON t.regno=r.regno 
+-- USING(regno)
+WHERE pd.ticketsRcvd >=3 
+AND violation LIKE '%red light%'
 ;
+
+-- SELECT * FROM personDetails;
